d1ad05c11f83544e89e81184ac64afa10f106d18
CHUANG DENG <cdengcnc@sjtu.edu.cn>
2020-07-09 22:17:16 +0800

    add a new project: v3

---
+++ v3/fala_rd_st_ex/rd_st_ex.py
@@ -0,0 +1,175 @@
+import numpy as np
+import pandas as pd
+import os
+import math
+import datetime
+from sympy import Matrix
+from scipy.linalg import null_space
+
+from multiprocessing import Pool
+
+
+# def valid_s(s_value):
+#     if s_value < 0.0:
+#         s_new = 0.0
+#     elif s_value > 1.0:
+#         s_new = 1.0
+#     else:
+#         s_new = s_value
+#     return s_new
+
+
+def build_markov_chain(qvec, p, q):
+    m = np.array([[qvec[0] * p[0] * q[0], qvec[0] * p[0] * (1 - q[0]), qvec[0] * (1 - p[0]) * q[0],
+                   qvec[0] * (1 - p[0]) * (1 - q[0]),
+                   (1 - qvec[0]) * p[4] * q[4], (1 - qvec[0]) * p[4] * (1 - q[4]), (1 - qvec[0]) * (1 - p[4]) * q[4],
+                   (1 - qvec[0]) * (1 - p[4]) * (1 - q[4])],
+                  [qvec[1] * p[1] * q[1], qvec[1] * p[1] * (1 - q[1]), qvec[1] * (1 - p[1]) * q[1],
+                   qvec[1] * (1 - p[1]) * (1 - q[1]),
+                   (1 - qvec[1]) * p[5] * q[5], (1 - qvec[1]) * p[5] * (1 - q[5]), (1 - qvec[1]) * (1 - p[5]) * q[5],
+                   (1 - qvec[1]) * (1 - p[5]) * (1 - q[5])],
+                  [qvec[2] * p[2] * q[2], qvec[2] * p[2] * (1 - q[2]), qvec[2] * (1 - p[2]) * q[2],
+                   qvec[2] * (1 - p[2]) * (1 - q[2]),
+                   (1 - qvec[2]) * p[6] * q[6], (1 - qvec[2]) * p[6] * (1 - q[6]), (1 - qvec[2]) * (1 - p[6]) * q[6],
+                   (1 - qvec[2]) * (1 - p[6]) * (1 - q[6])],
+                  [qvec[3] * p[3] * q[3], qvec[3] * p[3] * (1 - q[3]), qvec[3] * (1 - p[3]) * q[3],
+                   qvec[3] * (1 - p[3]) * (1 - q[3]),
+                   (1 - qvec[3]) * p[7] * q[7], (1 - qvec[3]) * p[7] * (1 - q[7]), (1 - qvec[3]) * (1 - p[7]) * q[7],
+                   (1 - qvec[3]) * (1 - p[7]) * (1 - q[7])],
+                  [qvec[4] * p[0] * q[0], qvec[4] * p[0] * (1 - q[0]), qvec[4] * (1 - p[0]) * q[0],
+                   qvec[4] * (1 - p[0]) * (1 - q[0]),
+                   (1 - qvec[4]) * p[4] * q[4], (1 - qvec[4]) * p[4] * (1 - q[4]), (1 - qvec[4]) * (1 - p[4]) * q[4],
+                   (1 - qvec[4]) * (1 - p[4]) * (1 - q[4])],
+                  [qvec[5] * p[1] * q[1], qvec[5] * p[1] * (1 - q[1]), qvec[5] * (1 - p[1]) * q[1],
+                   qvec[5] * (1 - p[1]) * (1 - q[1]),
+                   (1 - qvec[5]) * p[5] * q[5], (1 - qvec[5]) * p[5] * (1 - q[5]), (1 - qvec[5]) * (1 - p[5]) * q[5],
+                   (1 - qvec[5]) * (1 - p[5]) * (1 - q[5])],
+                  [qvec[6] * p[2] * q[2], qvec[6] * p[2] * (1 - q[2]), qvec[6] * (1 - p[2]) * q[2],
+                   qvec[6] * (1 - p[2]) * (1 - q[2]),
+                   (1 - qvec[6]) * p[6] * q[6], (1 - qvec[6]) * p[6] * (1 - q[6]), (1 - qvec[6]) * (1 - p[6]) * q[6],
+                   (1 - qvec[6]) * (1 - p[6]) * (1 - q[6])],
+                  [qvec[7] * p[3] * q[3], qvec[7] * p[3] * (1 - q[3]), qvec[7] * (1 - p[3]) * q[3],
+                   qvec[7] * (1 - p[3]) * (1 - q[3]),
+                   (1 - qvec[7]) * p[7] * q[7], (1 - qvec[7]) * p[7] * (1 - q[7]), (1 - qvec[7]) * (1 - p[7]) * q[7],
+                   (1 - qvec[7]) * (1 - p[7]) * (1 - q[7])]])
+    return m
+
+
+def average_game(s_n, qvec, pl, ql, f_p, f_q):
+    average_payoff = []
+    for i in range(s_n):
+        average_payoff.append([[0 for _ in range(4)], [0 for _ in range(4)]])
+    for s_i in range(s_n):
+        for a_i in [1, 0]:
+            for a_j in [1, 0]:
+                pa = pl[:]
+                qa = ql[:]
+                for s_j in range(4):
+                    pa[s_i * 4 + s_j] = a_i
+                    qa[s_i * 4 + s_j] = a_j
+                m = build_markov_chain(qvec, pa, qa)
+                null_matrix = np.transpose(m) - np.eye(8)
+                v = null_space(null_matrix)
+                v = v / np.sum(v)
+                r_p = np.dot(f_p, v)[0][0]
+                r_q = np.dot(f_q, v)[0][0]
+                average_payoff[s_i][0][(1 - a_i) * 2 + (1 - a_j)] = r_p
+                average_payoff[s_i][1][(1 - a_i) * 2 + (1 - a_j)] = r_q
+    m = build_markov_chain(qvec, pl, ql)
+    null_matrix = np.transpose(m) - np.eye(8)
+    v = null_space(null_matrix)
+    v = v / np.sum(v)
+    v = np.array(v)
+    v = v.transpose()[0]
+    return v, average_payoff
+
+
+def build_payoff_matrix(s_i, average_payoff, id='p'):
+    if id == 'p':
+        apm = np.array([[average_payoff[s_i][0][0], average_payoff[s_i][0][1]],
+                        [average_payoff[s_i][0][2], average_payoff[s_i][0][3]]])
+    else:
+        apm = np.array([[average_payoff[s_i][1][0], average_payoff[s_i][1][2]],
+                        [average_payoff[s_i][1][1], average_payoff[s_i][1][3]]])
+    return apm
+
+
+def evolve(s_n, average_payoff, p0, q0, p1, q1, step_size, v):
+    for s_i in range(s_n):
+        p_m = build_payoff_matrix(s_i, average_payoff, id='p')
+        q_m = build_payoff_matrix(s_i, average_payoff, id='q')
+        if s_i == 0:
+            p_o = np.dot(p_m, [[q0], [1 - q0]])
+            q_o = np.dot(q_m, [[p0], [1 - p0]])
+            v_s = np.sum(v[0:4])
+            dp = (np.dot([1, 0], p_o)[0] - np.dot([p0, 1 - p0], p_o)[0]) * p0 * v_s
+            dq = (np.dot([1, 0], q_o)[0] - np.dot([q0, 1 - q0], q_o)[0]) * q0 * v_s
+            p0 = p0 + dp * step_size
+            q0 = q0 + dq * step_size
+        else:
+            p_o = np.dot(p_m, [[q1], [1 - q1]])
+            q_o = np.dot(q_m, [[p1], [1 - p1]])
+            v_s = np.sum(v[4:8])
+            dp = (np.dot([1, 0], p_o)[0] - np.dot([p1, 1 - p1], p_o)[0]) * p1 * v_s
+            dq = (np.dot([1, 0], q_o)[0] - np.dot([q1, 1 - q1], q_o)[0]) * q1 * v_s
+            p1 = p1 + dp * step_size
+            q1 = q1 + dq * step_size
+    return p0, q0, p1, q1
+
+
+def run_task_rd(s_init):
+    t = np.arange(0, 10e5)
+    step_size = 0.001
+    s_n = 2
+    print(s_init)
+    for z_1, z_2 in [[0.9, 0.1]]:
+        p0 = s_init[0]
+        q0 = s_init[1]
+        p1 = s_init[2]
+        q1 = s_init[3]
+        print(z_1, z_2)
+        # qvec = [z_1, z_2, z_2, z_2, z_1, z_2, z_2, z_2]
+        # qvec = [0.7, 0.9, 0.3, 0.5, 0.5, 0.7, 0.1, 0.3]
+        qvec = [27/40, 6/40, 39/40, 18/40, 23/40, 2/40, 35/40, 14/40]
+        # f_p = np.array([3, 1, 4, 2, 3, 1, 4, 2])
+        f_p = np.array([3, 1, 4, 2, 7, 5, 8, 6])
+        f_p = f_p.reshape(f_p.size, 1).transpose()
+        # f_q = np.array([3, 4, 1, 2, 3, 4, 1, 2])
+        f_q = np.array([3, 4, 1, 2, 7, 8, 5, 6])
+        f_q = f_q.reshape(f_q.size, 1).transpose()
+        d = []
+        d.append([p0, q0, p1, q1])
+        for _ in t:
+            if _ % 1000 == 0:
+                print('rd', _)
+            pl = [p0, p0, p0, p0, p1, p1, p1, p1]
+            ql = [q0, q0, q0, q0, q1, q1, q1, q1]
+            v, average_payoff = average_game(s_n, qvec, pl, ql, f_p, f_q)
+            p0, q0, p1, q1 = evolve(s_n, average_payoff, p0, q0, p1, q1, step_size, v)
+            d.append([p0, q0, p1, q1])
+        abs_path = os.path.abspath(os.path.join(os.getcwd(), "./results_st_ex"))
+        csv_file_name = "/rd_%.2f_%.2f_%.2f_%.2f_strategy_trace.csv" % (s_init[0], s_init[1], s_init[2], s_init[3])
+        file_name = abs_path + csv_file_name
+        d_pd = pd.DataFrame(d)
+        d_pd.to_csv(file_name, index=None)
+
+
+def read_s_init():
+    abs_path = os.getcwd()
+    dir_name = os.path.join(abs_path)
+    f = os.path.join(dir_name, "s_init_file.csv")
+    data = pd.read_csv(f, usecols=['0', '1', '2', '3'])
+    s_init = np.array(data).tolist()
+    return s_init
+
+
+if __name__ == '__main__':
+    s_init_list = read_s_init()
+    init_num = len(s_init_list)
+    p_rd = Pool()
+    for _ in range(init_num):
+        s_init = s_init_list[_][:]
+        p_rd.apply_async(run_task_rd, args=(s_init,))
+    p_rd.close()
+    p_rd.join()
+    print("All subprocesses done")
---
+++ v3/fala_rd_st_sp/fala_st_sp.py
@@ -0,0 +1,201 @@
+import numpy as np
+import random
+import pandas as pd
+import os
+import math
+from copy import deepcopy
+from sympy import Matrix
+from scipy.linalg import null_space
+
+from multiprocessing import Pool
+
+
+pd_game_1 = [[2, 2], [4, 1], [1, 4], [3, 3]]
+# pd_game_2 = [[2, 2], [4, 1], [1, 4], [3, 3]]
+pd_game_2 = [[6, 6], [8, 5], [5, 8], [7, 7]]
+
+# transition_prob = [0.1, 0.1, 0.1, 0.9, 0.1, 0.1, 0.1, 0.9] # asymmetric
+# transition_prob = [0.5, 0.3, 0.9, 0.7, 0.3, 0.1, 0.7, 0.5] # zero-determinant
+# transition_prob = [18/40, 39/40, 6/40, 27/40, 14/40, 35/40, 2/40, 23/40] # exploitation
+transition_prob = [27/40, 39/40, 6/40, 18/40, 23/40, 35/40, 2/40, 14/40]
+
+
+def play_pd_game_1(a_x, a_y):
+    return pd_game_1[a_x * 2 + a_y]
+
+
+def play_pd_game_2(a_x, a_y):
+    return pd_game_2[a_x * 2 + a_y]
+
+
+def next_state(s, a_x, a_y):
+    prob = transition_prob[s * 4 + a_x * 2 + a_y]
+    if random.random() < prob:
+        s_ = 0
+    else:
+        s_ = 1
+    return s_
+
+
+# def valid_s(s_value):
+#     if s_value < 0.0:
+#         s_new = 0.0
+#     elif s_value > 1.0:
+#         s_new = 1.0
+#     else:
+#         s_new = s_value
+#     return s_new
+
+
+class Agent:
+    def __init__(self, alpha, agent_id):
+        self.id = agent_id
+        self.strategy = []
+        self.actions = [0, 1]  # 0 for defection and 1 for cooperation
+        self.states = [0, 1]  # there are two state: 0 and 1
+        self.len_action = len(self.actions)
+        self.alpha = alpha
+        self.strategy_trace = []
+
+    def get_strategy(self):
+        return self.strategy
+
+    def initial_strategy(self):
+        for state in self.states:
+            self.strategy.append([1 / self.len_action for _ in range(self.len_action)])
+
+    def set_strategy(self, new_stra=None):
+        """
+        set a specified strategy
+        :param new_s: the probability to play cooperation (action[1])
+        :return:
+        """
+        if new_stra:
+            for state in self.states:
+                self.strategy[state] = new_stra[state]
+
+    def choose_action(self, s):
+        # print(self.id, self.strategy)
+        a = np.random.choice(np.array(self.actions), size=1, p=self.strategy[s])[0]
+        return a
+
+    def update_strategy(self, s, a, r):
+        for action in self.actions:
+            if action == a:
+                self.strategy[s][action] = self.strategy[s][action] + self.alpha * r * (1 - self.strategy[s][action])
+            else:
+                self.strategy[s][action] = self.strategy[s][action] - self.alpha * r * self.strategy[s][action]
+
+    def record_strategy(self):
+        self.strategy_trace.append(deepcopy(self.strategy))
+
+
+def run_game_fala(agent_x_init_strategy, agent_y_init_strategy, s_0):
+    agent_x = Agent(alpha=0.00001, agent_id=0)
+    agent_y = Agent(alpha=0.00001, agent_id=1)
+    agent_x.initial_strategy()
+    agent_y.initial_strategy()
+    agent_x.set_strategy(agent_x_init_strategy)
+    agent_y.set_strategy(agent_y_init_strategy)
+    cur_s = s_0
+    games = [play_pd_game_1, play_pd_game_2]
+    r_sum_x = np.array([0.0, 0.0])  # store the sum of reward of agent_x for each state: state 0 and state 1
+    r_sum_y = np.array([0.0, 0.0])
+    tau_x = np.array([0.0, 0.0])  # store the average reward of agent_x for each state: state 0 and state 1
+    tau_y = np.array([0.0, 0.0])
+    action_t_x = np.array([0, 0])  # for agent_x, the last action taken in state 0 and state 1
+    action_t_y = np.array([0, 0])
+    time_step = np.array([0, 0])  # store the sum of time since last time meeting each state: state 0 and state 1
+    visited = [0, 0]
+    for _ in range(20000000):
+        if _ % 10000 == 0:
+            print('fala', _)
+        agent_x.record_strategy()
+        agent_y.record_strategy()
+        if visited[cur_s] == 0 or visited[1 - cur_s] == 0:
+            if visited[cur_s] == 0:
+                visited[cur_s] = 1
+                a_x = agent_x.choose_action(cur_s)
+                a_y = agent_y.choose_action(cur_s)
+                action_t_x[cur_s] = a_x
+                action_t_y[cur_s] = a_y
+                r_x, r_y = games[cur_s](a_x, a_y)
+                r_sum_x[cur_s] = r_sum_x[cur_s] + r_x
+                r_sum_y[cur_s] = r_sum_y[cur_s] + r_y
+                time_step[cur_s] += 1
+                cur_s = next_state(cur_s, a_x, a_y)
+            else:
+                tau_x[cur_s] = r_sum_x[cur_s] / time_step[cur_s]
+                tau_y[cur_s] = r_sum_y[cur_s] / time_step[cur_s]
+                agent_x.update_strategy(cur_s, action_t_x[cur_s], tau_x[cur_s])
+                agent_y.update_strategy(cur_s, action_t_y[cur_s], tau_y[cur_s])
+                r_sum_x[cur_s] = 0
+                r_sum_y[cur_s] = 0
+                time_step[cur_s] = 0
+                a_x = agent_x.choose_action(cur_s)
+                a_y = agent_y.choose_action(cur_s)
+                action_t_x[cur_s] = a_x
+                action_t_y[cur_s] = a_y
+                r_x, r_y = games[cur_s](a_x, a_y)
+                r_sum_x[cur_s] = r_sum_x[cur_s] + r_x
+                r_sum_y[cur_s] = r_sum_y[cur_s] + r_y
+                time_step[cur_s] += 1
+                cur_s = next_state(cur_s, a_x, a_y)
+        else:
+            tau_x[cur_s] = r_sum_x[cur_s] / time_step[cur_s]
+            tau_y[cur_s] = r_sum_y[cur_s] / time_step[cur_s]
+            agent_x.update_strategy(cur_s, action_t_x[cur_s], tau_x[cur_s])
+            agent_y.update_strategy(cur_s, action_t_y[cur_s], tau_y[cur_s])
+            r_sum_x[cur_s] = 0
+            r_sum_y[cur_s] = 0
+            time_step[cur_s] = 0
+            a_x = agent_x.choose_action(cur_s)
+            a_y = agent_y.choose_action(cur_s)
+            action_t_x[cur_s] = a_x
+            action_t_y[cur_s] = a_y
+            r_x, r_y = games[cur_s](a_x, a_y)
+            r_sum_x = r_sum_x + r_x
+            r_sum_y = r_sum_y + r_y
+            time_step = time_step + 1
+            cur_s = next_state(cur_s, a_x, a_y)
+
+    p = []
+    for i in range(len(agent_x.strategy_trace)):
+        p.append([agent_x.strategy_trace[i][0][1], agent_y.strategy_trace[i][0][1], agent_x.strategy_trace[i][1][1],
+                  agent_y.strategy_trace[i][1][1]])
+    return p, agent_x.strategy_trace, agent_y.strategy_trace
+
+
+def run_task_fala(s_init):
+    p0 = s_init[0]
+    q0 = s_init[1]
+    p1 = s_init[2]
+    q1 = s_init[3]
+    p, x_st, y_st = run_game_fala(agent_x_init_strategy=[[1 - p0, p0], [1 - p1, p1]],
+                                  agent_y_init_strategy=[[1 - q0, q0], [1 - q1, q1]], s_0=0)
+    abs_path = os.path.abspath(os.path.join(os.getcwd(), "./results_st_sp"))
+    csv_file_name = "/fala_%.2f_%.2f_%.2f_%.2f_strategy_trace.csv" % (p0, q0, p1, q1)
+    file_name = abs_path + csv_file_name
+    d_pd = pd.DataFrame(p)
+    d_pd.to_csv(file_name, index=None)
+
+
+def read_s_init():
+    abs_path = os.getcwd()
+    dir_name = os.path.join(abs_path)
+    f = os.path.join(dir_name, "s_init_file.csv")
+    data = pd.read_csv(f, usecols=['0', '1', '2', '3'])
+    s_init = np.array(data).tolist()
+    return s_init
+
+
+if __name__ == '__main__':
+    p_fala = Pool()
+    s_init_list = read_s_init()
+    init_num = len(s_init_list)
+    for _ in range(init_num):
+        s_init = s_init_list[_][:]
+        p_fala.apply_async(run_task_fala, args=(s_init,))
+    p_fala.close()
+    p_fala.join()
+    print("All subprocesses done")
