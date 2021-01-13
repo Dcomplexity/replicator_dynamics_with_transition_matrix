a1621b8f43d850b54ca4e4306983ebe10bf274cf
Chuang Deng <dc199082@126.com>
2021-01-13 13:40:18 +0800

    add the repo named exp_diff_b_ts_at

---
+++ v5/exp_diff_b_ts_at/strd_ts_st_at.py
@@ -0,0 +1,201 @@
+import numpy as np
+import pandas as pd
+import os
+import math
+import datetime
+from sympy import Matrix
+from scipy.linalg import null_space
+
+from multiprocessing import Pool
+
+
+# def valid_s(s_value):
+#     if s_value < 0.0:
+#         s_new = 0.0
+#     elif s_value > 1.0:
+#         s_new = 1.0
+#     else:
+#         s_new = s_value
+#     return s_new
+
+
+def build_markov_chain(qvec, p, q):
+    m = np.ones((8, 8))
+    for i in range(np.size(m, axis=0)):
+        for j in range(np.size(m, axis=1)):
+            if j % 4 == 0:
+                m[i][j] = qvec[i][j // 4] * p[j // 4] * q[j // 4]
+            elif j % 4 == 1:
+                m[i][j] = qvec[i][j // 4] * p[j // 4] * (1 - q[j // 4])
+            elif j % 4 == 2:
+                m[i][j] = qvec[i][j // 4] * (1 - p[j // 4]) * q[j // 4]
+            else:
+                m[i][j] = qvec[i][j // 4] * (1 - p[j // 4]) * (1 - q[j // 4])
+    return m
+
+
+def calc_expected_payoff(qvec, pl, ql, f_p, f_q):
+    pa = pl[:]
+    qa = ql[:]
+    m = build_markov_chain(qvec, pa, qa)
+    null_matrix = np.transpose(m) - np.eye(8)
+    v = null_space(null_matrix)
+    v = v / np.sum(v)
+    r_p_e = np.dot(f_p, v)[0][0]
+    r_q_e = np.dot(f_q, v)[0][0]
+    return v, r_p_e, r_q_e
+
+
+def calc_payoff(agent_id, s, a_p, a_q, qvec, pl, ql, f_p, f_q):
+    action_payoff = 0
+    if agent_id == 0:
+        pa = pl[:]
+        qa = ql[:]
+        pa[s] = a_p
+        qa[s] = a_q
+        m = build_markov_chain(qvec, pa, qa)
+        null_matrix = np.transpose(m) - np.eye(8)
+        v = null_space(null_matrix)
+        v = v / np.sum(v)
+        action_payoff = np.dot(f_p, v)[0][0]
+        return action_payoff
+    elif agent_id == 1:
+        pa = pl[:]
+        qa = ql[:]
+        pa[s] = a_p
+        qa[s] = a_q
+        m = build_markov_chain(qvec, pa, qa)
+        null_matrix = np.transpose(m) - np.eye(8)
+        v = null_space(null_matrix)
+        v = v / np.sum(v)
+        action_payoff = np.dot(f_q, v)[0][0]
+        return action_payoff
+
+
+def run_task_rd(s_init):
+    t = np.arange(0, int(10e5))
+    step_size = 0.001
+    s_n = 2
+    print(s_init)
+    for z_1, z_2 in [[0.9, 0.1]]:
+        p0 = s_init[0]
+        q0 = s_init[1]
+        p1 = s_init[2]
+        q1 = s_init[3]
+        print(z_1, z_2)
+        qmatrix = [[0.9, 0.1], [0.1, 0.9], [0.1, 0.9], [0.1, 0.9],
+                   [0.9, 0.1], [0.1, 0.9], [0.1, 0.9], [0.1, 0.9]]
+        qmatrix0 = [[1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [1.0, 0.0],
+                    [1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [1.0, 0.0]]
+        qmatrix1 = [[0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0],
+                    [0.0, 1.0], [0.0, 1.0], [0.0, 1.0], [0.0, 1.0]]
+        # f_p = np.array([3, 0, 10, 2, 4, 0, 10, 1])
+        f_p = np.array([1.0, -1, 2.0, 0, 0.2, -1, 1.2, 0])
+        f_p = f_p.reshape(f_p.size, 1).transpose()
+        # f_q = np.array([3, 10, 0, 2, 4, 10, 0, 1])
+        f_q = np.array([1.0, 2.0, -1, 0, 0.2, 1.2, -1, 0])
+        f_q = f_q.reshape(f_q.size, 1).transpose()
+        d = []
+        d.append([p0, q0, p1, q1])
+        for _ in t:
+            if _ % 1000 == 0:
+                print('strd', _)
+            pl = [p0, p1]
+            ql = [q0, q1]
+            v, r_p_e, r_q_e = calc_expected_payoff(qmatrix, pl, ql, f_p, f_q)
+            r_p_0_cc = calc_payoff(0, 0, 1, 1, qmatrix, pl, ql, f_p, f_q)
+            r_q_0_cc = calc_payoff(1, 0, 1, 1, qmatrix, pl, ql, f_p, f_q)
+            r_p_0_cd = calc_payoff(0, 0, 1, 0, qmatrix, pl, ql, f_p, f_q)
+            r_q_0_cd = calc_payoff(1, 0, 1, 0, qmatrix, pl, ql, f_p, f_q)
+            r_p_0_dc = calc_payoff(0, 0, 0, 1, qmatrix, pl, ql, f_p, f_q)
+            r_q_0_dc = calc_payoff(1, 0, 0, 1, qmatrix, pl, ql, f_p, f_q)
+            r_p_0_dd = calc_payoff(0, 0, 0, 0, qmatrix, pl, ql, f_p, f_q)
+            r_q_0_dd = calc_payoff(1, 0, 0, 0, qmatrix, pl, ql, f_p, f_q)
+            r_p_1_cc = calc_payoff(0, 1, 1, 1, qmatrix, pl, ql, f_p, f_q)
+            r_q_1_cc = calc_payoff(1, 1, 1, 1, qmatrix, pl, ql, f_p, f_q)
+            r_p_1_cd = calc_payoff(0, 1, 1, 0, qmatrix, pl, ql, f_p, f_q)
+            r_q_1_cd = calc_payoff(1, 1, 1, 0, qmatrix, pl, ql, f_p, f_q)
+            r_p_1_dc = calc_payoff(0, 1, 0, 1, qmatrix, pl, ql, f_p, f_q)
+            r_q_1_dc = calc_payoff(1, 1, 0, 1, qmatrix, pl, ql, f_p, f_q)
+            r_p_1_dd = calc_payoff(0, 1, 0, 0, qmatrix, pl, ql, f_p, f_q)
+            r_q_1_dd = calc_payoff(1, 1, 0, 0, qmatrix, pl, ql, f_p, f_q)
+            v_0 = np.sum(v[0:4])
+            v_1 = np.sum(v[4:8])
+            # f_p_exp = np.array([r_p_0_cc, r_p_0_cd, r_p_0_dc, r_p_0_dd, r_p_1_cc, r_p_1_cd, r_p_1_dc, r_p_1_dd])
+            # f_p_exp = f_p_exp.reshape(f_p_exp.size, 1).transpose()
+            # f_q_exp = np.array([r_q_0_cc, r_q_0_cd, r_q_0_dc, r_q_0_dd, r_q_1_cc, r_q_1_cd, r_q_1_dc, r_q_1_dd])
+            # f_q_exp = f_q_exp.reshape(f_q_exp.size, 1).transpose()
+            dp0 = ((r_p_0_cc * q0 + r_p_0_cd * (1 - q0)) - (p0 * (r_p_0_cc * q0 + r_p_0_cd * (1 - q0)) + (1 - p0) * (
+                    r_p_0_dc * q0 + r_p_0_dd * (1 - q0)))) * p0 * v_0
+            dq0 = ((r_q_0_cc * p0 + r_q_0_dc * (1 - p0)) - (q0 * (r_q_0_cc * p0 + r_q_0_dc * (1 - p0)) + (1 - q0) * (
+                    r_q_0_cd * p0 + r_q_0_dd * (1 - p0)))) * q0 * v_0
+            dp1 = ((r_p_1_cc * q1 + r_p_1_cd * (1 - q1)) - (p1 * (r_p_1_cc * q1 + r_p_1_cd * (1 - q1)) + (1 - p1) * (
+                    r_p_1_dc * q1 + r_p_1_dd * (1 - q1)))) * p1 * v_1
+            dq1 = ((r_q_1_cc * p1 + r_q_1_dc * (1 - p1)) - (q1 * (r_q_1_cc * p1 + r_q_1_dc * (1 - p1)) + (1 - q1) * (
+                    r_q_1_cd * p1 + r_q_1_dd * (1 - p1)))) * q1 * v_1
+            # v_avg = [p0 * q0, p0 * (1 - q0), (1 - p0) * q0, (1 - p0) * (1 - q0), p1 * q1, p1 * (1 - q1), (1 - p1) * q1,
+            #          (1 - p1) * (1 - q1)]
+            # v_p0 = [1 * q0, 1 * (1 - q0), (1 - 1) * q0, (1 - 1) * (1 - q0), p1 * q1, p1 * (1 - q1), (1 - p1) * q1,
+            #          (1 - p1) * (1 - q1)]
+            # v_q0 = [p0 * 1, p0 * (1 - 1), (1 - p0) * 1, (1 - p0) * (1 - 1), p1 * q1, p1 * (1 - q1), (1 - p1) * q1,
+            #          (1 - p1) * (1 - q1)]
+            # v_p1 = [p0 * q0, p0 * (1 - q0), (1 - p0) * q0, (1 - p0) * (1 - q0), 1 * q1, 1 * (1 - q1), (1 - 1) * q1,
+            #          (1 - 1) * (1 - q1)]
+            # v_q1 = [p0 * q0, p0 * (1 - q0), (1 - p0) * q0, (1 - p0) * (1 - q0), p1 * 1, p1 * (1 - 1), 1 * (1 - p1),
+            #          (1 - p1) * (1 - 1)]
+            # r_p_avg = np.dot(v_avg, f_p_exp)
+            # r_q_avg = np.dot(v_avg, f_q_exp)
+            # dp0 = (calc_payoff(0, 0, 1, q0, qmatrix0, pl, ql, f_p_exp, f_q_exp) - calc_payoff(0, 0, p0, q0, qmatrix0,
+            #                                                                                   pl, ql,
+            #                                                                                   f_p_exp,
+            #                                                                                   f_q_exp)) * p0 * v_0
+            # dq0 = (calc_payoff(1, 0, p0, 1, qmatrix0, pl, ql, f_p_exp, f_q_exp) - calc_payoff(1, 0, p0, q0, qmatrix0,
+            #                                                                                   pl, ql,
+            #                                                                                   f_p_exp,
+            #                                                                                   f_q_exp)) * q0 * v_0
+            # dp1 = (calc_payoff(0, 1, 1, q1, qmatrix1, pl, ql, f_p_exp, f_q_exp) - calc_payoff(0, 1, p1, q1, qmatrix1,
+            #                                                                                   pl, ql,
+            #                                                                                   f_p_exp,
+            #                                                                                   f_q_exp)) * p1 * v_1
+            # dq1 = (calc_payoff(1, 1, p1, 1, qmatrix1, pl, ql, f_p_exp, f_q_exp) - calc_payoff(1, 1, p1, q1, qmatrix1,
+            #                                                                                   pl, ql,
+            #                                                                                   f_p_exp,
+            #                                                                                   f_q_exp)) * q1 * v_1
+            # dp0 = (np.dot(v_p0, f_p_exp) - r_p_avg) * p0 * v_0
+            # dq0 = (np.dot(v_q0, f_q_exp) - r_q_avg) * q0 * v_0
+            # dp1 = (np.dot(v_p1, f_p_exp) - r_p_avg) * p1 * v_1
+            # dq1 = (np.dot(v_q1, f_q_exp) - r_q_avg)
+            p0 = p0 + dp0 * step_size
+            q0 = q0 + dq0 * step_size
+            p1 = p1 + dp1 * step_size
+            q1 = q1 + dq1 * step_size
+            d.append([p0, q0, p1, q1])
+        abs_path = os.path.abspath(os.path.join(os.getcwd(), "results_st_at"))
+        csv_file_name = "/strd_ts_st_at_%.2f_%.2f_%.2f_%.2f_strategy_trace.csv" % (
+            s_init[0], s_init[1], s_init[2], s_init[3])
+        file_name = abs_path + csv_file_name
+        d_pd = pd.DataFrame(d)
+        d_pd.to_csv(file_name, index=None)
+
+
+def read_s_init():
+    abs_path = os.getcwd()
+    dir_name = os.path.join(abs_path)
+    f = os.path.join(dir_name, "s_init_file.csv")
+    data = pd.read_csv(f, usecols=['0', '1', '2', '3'])
+    s_init = np.array(data).tolist()
+    return s_init
+
+
+if __name__ == '__main__':
+    s_init_list = read_s_init()
+    init_num = len(s_init_list)
+    p_rd = Pool()
+    for _ in range(init_num):
+        s_init = s_init_list[_][:]
+        p_rd.apply_async(run_task_rd, args=(s_init,))
+    p_rd.close()
+    p_rd.join()
+    print("All subprocesses done")
+    # run_task_rd(s_init_list[0])
+    # run_task()
